21b16ea74edbc0ba01dc4499d5c0abf46d03a7c8
Aahilyn <57688825+Aahilyn@users.noreply.github.com>
2019-11-16 16:50:44 -0300

    Add files via upload

--- bombajeringa1.ino
+++ bombajeringa1.ino
@@ -1,85 +1,129 @@
 //Declarar variables
-//  Variables de tiempo
 
-double duty; //porcentaje de tiempo de encendido del pin con respecto al periodo
-double pinON; //tiempo de encendido del pin
-double pinOFF; // tiempo de apagado del pin
-//  Pines
-const int dir = 2;
-const int stp = 3;
-//const int MS2=4;
-const int MS1 = 5;
-const int EN = 6;
+/* Variables de forma de onda cuadrada */
+// Porcentaje de tiempo de encendido del pin con respecto al periodo
+// Puede darle más fuerza al paso (entre 0 y 1)
+double duty;        
 
-//Variables
+double pinON;       // Tiempo de encendido del pin 
+double pinOFF;      // Tiempo de apagado del pin
 
-unsigned long t_ejec; // en milisec
-double periodo_paso; // Periodo entre paso y paso, o periodo de la onda generada
 
-int Q; // caudal
-int vol; //en ml
-const int R; //volumen de jeringa recorrido cuando el motor da 1 paso
+/*  Pines */
+const int dir = 2;  // Pin de direccion
+const int stp = 3;  // Pin de paso (acción de dar un paso)
+//const int MS2=4;  // Pin de segunda precision
+const int MS1 = 5;  // Pin de primera precision
+const int EN = 6;   // Pin de ENABLE
 
-int aux;
+/*Variables de tiempo*/
+// Tiempo de ejecucion total del arduino, en milisec, puede almacenar un máximo de 50 dias
+unsigned long t_ejec;
 
-void setup() {
+// Tiempo de ejecucion continuo del arduino, en milisec (solo en la funcion pulsado)
+unsigned long t_cont;
+// Tiempo de ejecucion apagado del arduino, en milisec (solo en la funcion pulsado)
+unsigned long t_apag;
+
+
+// Periodo entre paso y paso, o periodo de la onda generada
+// Es inversamente proporcional a la velocidad del motor
+double periodo_paso; 
+
+/* Variable auxiliar*/
+// Variable auxiliar de encendido o apagado del dispositivo
+// Si vale 0, se encenderá, caso contrario se apagará
+int encendido; 
+ 
+void setup()
+{
   pinMode(stp, OUTPUT);
   pinMode(dir, OUTPUT);
   pinMode(MS1, OUTPUT);
   //  pinMode(MS2, OUTPUT);
-  pinMode(EN, OUTPUT);
-  resetBEDPins(); //Colocar pines en su estado por defecto
+  pinMode(EN, OUTPUT); // En LOW, habilita los FETs, para poder controlar el motor, caso contrario en HIGH
+  resetPins(); //Colocar pines en su estado por defecto
+    
   Serial.begin(9600); //Abrir comunicación serial (USB)
-  //valores()
-  t_ejec = 60000; // en milisegundos, 60000 es 1 minuto
-  periodo_paso = 20; // en milisegundos(como la velocidad)
-  //periodo_paso = 1 / (Q * R); //en segundos/pasos
-  duty = 0.5; //porcentaje
+    
+  // ENTRADA Y SALIDA
+  t_ejec = 60000*10; // en milisegundos, 60000 es 1 minuto
+  t_cont = 15000;
+  t_apag = 5000;
+  periodo_paso = 100; // en milisegundos, 20 default
+  duty = 0.3; //porcentaje
+  
   pinON = duty * periodo_paso;
   pinOFF = (1 - duty) * periodo_paso;
-  aux = 0;
+  encendido = 0;
 }
 
 //Main loop
-void loop() {
-  if (aux == 0) {
-    pulsado();// Hacer 1000 pasos
+void loop()
+{
+  if (encendido == 0) {  
+    Serial.println("Inicio");
+    
+    Serial.print("t_ejec: ");
+    Serial.println(t_ejec);
+    
+    Serial.print("t_cont: ");
+    Serial.println(t_cont);
+    
+    Serial.print("t_apag: ");
+    Serial.println(t_apag);
+
+    Serial.print("periodo_paso: ");
+    Serial.println(periodo_paso);
+
+    Serial.print("duty: ");
+    Serial.println(duty);
+    
     //continuo(t_ejec);
-    aux = 1;
+    //pulsado(t_ejec,t_cont,t_apag)
+    
+    encendido = 1; //Para ejecutar sólo una ves, lo colocamos igual a 1
+    Serial.println("Fin");
   }
 }
 
-void continuo(unsigned long t_ejeco) {
-  digitalWrite(EN, LOW); // En low, habilita los FETs, para poder controlar el motor
-  unsigned long startMillis = millis(); //empezamos a medir tiempo
-  while (millis() - startMillis < t_ejeco) {
+// Funcion que realiza un movimiento continuo
+void continuo(unsigned long t_ejeco) 
+{
+  digitalWrite(EN, LOW); // Habilitar motores
+  unsigned long startMillis = millis(); // Empezamos a medir tiempo
+  
+  // Comparamos el tiempo de ejecucion con la diferencia de tiempo entre que se empieza a ejecutar Arduino
+  // y el tiempo actual
+  while ( millis() - startMillis < t_ejeco) { 
+    // Dar un paso
     digitalWrite(stp, HIGH);
     wait(pinON);
     digitalWrite(stp, LOW);
     wait(pinOFF);
   }
-  digitalWrite(EN, HIGH); //Apagar FETs
+  digitalWrite(EN, HIGH); // Deshabilitar motores
 }
 
-void pulsado() {
-  unsigned long t_cont;
-  unsigned long t_apag;
-  unsigned long startMillis;
-  // Darle valores a t_cont y t_apag en milisegundos, ambos son entrada
-  t_cont = 15000;
-  t_apag = 5000;
-  digitalWrite(EN, LOW); // En low, habilita los FETs, para poder controlar el motor
-  startMillis = millis(); //empezamos a medir tiempo
-  while (millis() - startMillis < t_ejec) {
+// Funcion que realiza un movimiento alternado, entre continuo y apagado
+void pulsado(unsigned long t_ejeco, unsigned long t_cont, unsigned long t_apag)
+{ 
+  digitalWrite(EN, LOW); // Habilitar motores
+  unsigned long startMillis = millis(); // Empezamos a medir tiempo
+
+  // Comparamos el tiempo de ejecucion con la diferencia de tiempo entre que se empieza a ejecutar Arduino
+  // y el tiempo actual
+  while (millis() - startMillis < t_ejeco) {
     continuo(t_cont);
     wait(t_apag);
   }
+  digitalWrite(EN, HIGH); // Deshabilitar motores
 }
 
 void valores() {
   Serial.println("Ingrese el volumen");
   while (Serial.available() <= 0) {   // is a character available?
-    vol = Serial.read();
+    
   }
   Serial.println("Ingrese el tiempo de ejecucion");
   while (Serial.available() <= 0) {   // is a character available?
@@ -87,14 +131,17 @@ void valores() {
   }
 }
 
-void wait(unsigned long t_wait) {
+// Funcion que espera un tiempo determinado
+void wait(unsigned long t_wait) 
+{ 
   unsigned long startMillis=millis();
   while (millis() - startMillis < t_wait) {
- 
   }
 }
 
-void resetBEDPins() {
+// Funcion que resetea el estado de los pines
+void resetPins()
+{ 
   digitalWrite(stp, LOW);
   digitalWrite(dir, LOW);
   digitalWrite(MS1, HIGH); //en HIGH habilita micropasos
